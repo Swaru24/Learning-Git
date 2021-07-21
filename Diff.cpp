0a3e0528864fa0691e0b27d242290e5a32dadba1
Swarnima Gupta <guptaswarnima24@gmail.com>
2021-07-22 01:41:29 +0530

    added implementation for Diff.cpp

---
+++ Diff.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=15;
+    int b=10;
+    
+    int diff=0;
+    diff=a-b;
+    cout<<diff<<endl;
+    cout<<"My diff is"<<diff<<endl;
+    return 0;
+}
